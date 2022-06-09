8c7933447aa47b3d27f4d7e7cf4f959921aeae23
Cindy JEAN <cindy.jean83@gmail.com>
2022-06-09 12:20:31 +0200

    feat: added table plugin with mocked data

--- src/pages/employeeList/employeeList.jsx
+++ src/pages/employeeList/employeeList.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
 import React from "react";
-// import { Table } from "table";
+import { TablePlugin } from "table-plugin";
+import employees from "./employees.json";
 
+const headCells = [
+  { label: "First Name", data: "firstName" },
+  { label: "Last Name", data: "lastName" },
+  { label: "Start Date", data: "startDate" },
+  { label: "Department", data: "department" },
+  { label: "Date of Birth", data: "dateOfBirth" },
+  { label: "Street", data: "street" },
+  { label: "City", data: "city" },
+  { label: "State", data: "state" },
+  { label: "Zip Code", data: "zipCode" },
+];
 export default function EmployeeList() {
   return (
     <main className="container">
       <h1>Current employees</h1>
-      {/* <Table /> */}
+      <TablePlugin data={employees} headCells={headCells} />
       <Link to="/">Home</Link>
     </main>
   );
