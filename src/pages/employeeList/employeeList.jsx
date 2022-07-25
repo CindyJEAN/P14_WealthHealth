87b1a4bcd40ba71d2365f4e167c23f94bd32082f
Cindy JEAN <cindy.jean83@gmail.com>
2022-07-25 21:24:29 +0200

    fix: update table plugin (arrow icon)

--- src/pages/employeeList/employeeList.jsx
+++ src/pages/employeeList/employeeList.jsx
@@ -1,26 +1,31 @@
 import "./employeeList.css";
 import "table-plugin/dist/index.css";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import React from "react";
 import { TablePlugin } from "table-plugin";
-import employees from "./employees.json";
+// import employees from "./employees.json";
 
 const headCells = [
-  { label: "First Name", data: "firstName" },
-  { label: "Last Name", data: "lastName" },
-  { label: "Start Date", data: "startDate" },
-  { label: "Department", data: "department" },
-  { label: "Date of Birth", data: "dateOfBirth" },
-  { label: "Street", data: "street" },
-  { label: "City", data: "city" },
-  { label: "State", data: "state" },
-  { label: "Zip Code", data: "zipCode" },
+  { label: "First Name", data: "firstName", type: "text" },
+  { label: "Last Name", data: "lastName", type: "text" },
+  { label: "Start Date", data: "startDate", type: "date" },
+  { label: "Department", data: "department", type: "text" },
+  { label: "Date of Birth", data: "dateOfBirth", type: "date" },
+  { label: "Street", data: "street", type: "text" },
+  { label: "City", data: "city", type: "text" },
+  { label: "State", data: "state", type: "text" },
+  { label: "Zip Code", data: "zipCode", type: "text" },
 ];
 export default function EmployeeList() {
+  const [data, setData] = useState(
+    // @ts-ignore
+    JSON.parse(localStorage.getItem("employees")) || []
+  );
+
   return (
     <main className="container employeeListPage">
       <h1>Current employees</h1>
-      <TablePlugin data={employees} headCells={headCells} />
+      <TablePlugin data={data} headCells={headCells} />
       <Link to="/">Home</Link>
     </main>
   );
